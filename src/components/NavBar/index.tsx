3c56a34a2898de4154979bb364c0d6fb66ad6943
MaksKuchuk <maks.kuchuk.2016@gmail.com>
2022-09-29 22:52:56 +1000

    create main page

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./NavBar.module.scss";
+
+const NavBar: React.FC = () => {
+  return <div className={classes.container}></div>;
+};
+
+export default NavBar;
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./MainPage.module.scss";
+
+const MainPage = () => {
+  return <div className={classes.container}></div>;
+};
+
+export default MainPage;
